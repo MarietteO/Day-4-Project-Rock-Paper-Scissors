7d03d4bc393a9e4ba05301e13dc3a5c011d10e01
Mariette Olwagen <90972322+MarietteO@users.noreply.github.com>
2023-07-21 00:57:03 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,57 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+def choose_art(choice):
+    if choice == 0:
+        print(rock)
+    elif choice == 1:
+        print(paper)
+    else:
+        print(scissors)
+
+computer_wins = [(0, 1), (1, 2), (2, 0)]
+user_wins = [(0, 2), (1, 0), (2, 1)]
+draw = [(0, 0), (1, 1), (2, 2)]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors: "))
+computer_choice = random.randint(0, 2)
+combination = (user_choice, computer_choice)
+
+choose_art(user_choice)
+print("The computer chose:")
+choose_art(computer_choice)
+if combination in computer_wins:
+    print("Computer wins")
+elif combination in user_wins:
+    print("You win")
+else:
+    print("It's a draw")
+
+
+
